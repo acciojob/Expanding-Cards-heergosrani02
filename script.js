72aa189e024fbf8d0da8d46df101e7bdabba2d67
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-03-02 00:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let images = document.querySelectorAll('.panel');
+
+const expandImage = () => {
+    images.forEach((image) => {
+        image.addEventListener('click', () => {
+			images.forEach((img) => img.classList.remove('active'));
+            image.classList.add('active');
+        })
+    })
+}
+
+expandImage();
